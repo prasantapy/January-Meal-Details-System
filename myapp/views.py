af2afdd5a2dd090cf26f3c29aa9a4941c0383b81
prasanta sethi <prasanta.sethi.py@gmail.com>
2026-01-10 00:17:23 +0530

    Changes to be committed: 	modified:   myapp/views.py

--- myapp/views.py
+++ myapp/views.py
@@ -3,7 +3,7 @@ from myapp.models import Customer
 from django.http import HttpResponse
 
 def home(request):
-    return render(request,'Home.html')
+    return render(request,'home.html')
 def create_data(request):
     if request.method == 'POST':
         customers = Customer.objects.create(
